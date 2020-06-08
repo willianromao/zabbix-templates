4a4a70cf9467d20d1e16feb6075533b372fe88e6
WillianRomao <willian.romao@outlook.com.br>
2020-06-08 19:57:02 -0300

    remocao do script speedtest_cron.sh

--- Template SpeedTest v1/speedtest_cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# */1 * * * *	root	/root/scripts/speedtest_cron.sh
-
-INTERFACES=$(grep -i cron /root/scripts/speedtest_scan.sh | sed 's/# cron //gi')
-
-for int in `echo $INTERFACES`
-do
-/root/scripts/speedtest.sh $int
-done
